28a50f807aff2a6b7f04db8b260024c857fa3b00
injustice <injustice-x@users.noreply.github.com>
2025-02-26 21:28:26 +0530

    feat: added fileHandle

--- src/fileHandle.c
+++ src/fileHandle.c
@@ -2,6 +2,16 @@
 
 unsigned char *readFile(const char *filePath) {
   unsigned char *returnString;
+  FILE *f = fopen(filePath, "r");
+  if (f == NULL) {
+    perror("error in opening file");
+    return EXIT_FAILURE;
+  }
 
   return returnString;
+}
+
+int writeFile(const char *filePath, unsigned char *jsonString) {
+  FILE *f = fopen(filePath, "w");
+  return 0;
 }
