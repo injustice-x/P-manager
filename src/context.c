14cf2f4dd1426c4702c522617edb298471da3caa
injustice <injustice-x@users.noreply.github.com>
2025-03-10 17:51:43 +0530

    feat: changed structure

--- src/context.c
+++ src/context.c
@@ -1,54 +1,36 @@
 #include "../include/context.h"
-#include <stddef.h>
-#include <stdlib.h>
 
-passwordManagerContext *initPasswordManagerContext(const char *usersFilePath) {
-  passwordManagerContext *globalConetxt;
+passwordManagerContext *initPasswordManagerContext(const char *dataFilePath) {
 
-  if (usersFilePath == NULL) {
-    return NULL;
-  }
-  unsigned char *usersJason;
-  /*usersJason = readFile(usersFilePath);*/
-  if (usersJason == NULL) {
-
-    globalConetxt->users = malloc(sizeof(user));
-    globalConetxt->userCount = 1;
-    globalConetxt->usersFilePath = strdup(usersFilePath);
-    globalConetxt->currentUser = malloc(sizeof(userContext));
-  } else {
-    user *users;
-    users = unJsonUsers(usersJason);
-    size_t userCount;
-
-    globalConetxt->users = users;
-    globalConetxt->userCount = userCount;
-    globalConetxt->usersFilePath = strdup(usersFilePath);
-
-    globalConetxt->currentUser = malloc(sizeof(userContext));
-  }
-  if (globalConetxt->users == NULL) {
-    printf("error allocating memory:1");
+  if (dataFilePath == NULL) {
     return NULL;
   }
 
-  if (globalConetxt->currentUser == NULL) {
-    printf("error allocating memory:1");
+  passwordManagerContext *globalContext;
+  globalContext = malloc(sizeof(passwordManagerContext));
+
+  if (globalContext == NULL) {
+    free(globalContext);
     return NULL;
-  };
-  return globalConetxt;
+  }
+
+  globalContext->filePath = dataFilePath;
+  globalContext->currentUser = malloc(sizeof(user));
+
+  if (globalContext->currentUser == NULL) {
+    free(globalContext->currentUser);
+    free(globalContext);
+    return NULL;
+  }
+
+  return globalContext;
 }
 
 void freeGlobalContext(passwordManagerContext *globalContext) {
-  free(globalContext->users);
+  free(globalContext->currentUser->currentContext);
+  free(globalContext->currentUser->passwordHash);
+  free(globalContext->currentUser->usernameHash);
+
   free(globalContext->currentUser);
-  free(globalContext->usersFilePath);
   free(globalContext);
-}
-void freeUserContext(passwordManagerContext *globalContext) {
-  free(globalContext->currentUser->encryptionKey);
-  free(globalContext->currentUser->thisUser);
-  free(globalContext->currentUser->dataFilePath);
-  free(globalContext->currentUser->entries);
-  free(globalContext->currentUser);
 }
