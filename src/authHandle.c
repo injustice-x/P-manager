212b1ba8663aec8e565343b87dc0d03edde2efe4
injustice-x <sumanhosad98@gmail.com>
2025-03-12 17:45:52 +0530

    feat: addUser

--- src/authHandle.c
+++ src/authHandle.c
@@ -1,17 +1,30 @@
 #include "../include/context.h"
-#include <stdio.h>
 #include <stdlib.h>
 
-userContext *signUp(passwordManagerContext *globalContext) {
-  userContext *newUser;
-  newUser = malloc(sizeof(userContext));
-  char *username, *password;
-  printf("Enter username:");
-  scanf("%s", username);
-  printf("Enter password:");
-  scanf("%s", password);
-  globalContext->currentUser = newUser;
-  addUser(globalContext);
+#define MAX_SIZE 50
+#define DIGEST_SIZE 32 // SHA3-256 produces 32 bytes
 
-  return newUser;
-};
+int addUser(passwordManagerContext *globalContext) {
+  char *username = malloc(MAX_SIZE);
+  char *password = malloc(MAX_SIZE);
+  unsigned int usernameHashLen = 0;
+  unsigned int passwordHashLen = 0;
+
+  hashes *hash = globalContext->currentUser->hash;
+  hash->passwordHash = malloc(DIGEST_SIZE);
+  hash->usernameHash = malloc(DIGEST_SIZE);
+
+  printf("Enter the username: ");
+  scanf("%49s", username); // %49s to prevent buffer overflow
+  hash->usernameHash = hashIt(username, &usernameHashLen);
+
+  printf("Enter the password: ");
+  scanf("%49s", password);
+  hash->passwordHash = hashIt(password, &passwordHashLen);
+  int yes = writeHashes(hash, globalContext->filePath);
+
+  printf("\nUsername: %s\nPassword: %s\n", username, password);
+  free(username);
+  free(password);
+  return 0;
+}
